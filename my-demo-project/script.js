4cedeb97b7a4a2604da495170fd5970f93996447
Prajwal246800 <ankithgowda24680@gmail.com>
2024-02-20 18:37:34 +0530

    Basic Navbar

---
+++ my-demo-project/script.js
@@ -0,0 +1,6 @@
+document.getElementById("toggle").addEventListener("click", function(){
+    if(document.getElementById("nav-bar").style.display == "none")
+        document.getElementById("nav-bar").style.display = "block";
+    else
+    document.getElementById("nav-bar").style.display = "none"
+});
