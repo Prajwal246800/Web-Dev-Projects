d0663f14f5adc24d562ab3e9cab4e99cb296a96c
Prajwal246800 <ankithgowda24680@gmail.com>
2024-03-05 00:34:45 +0530

    Deleted Old Files Which Were Done For Portfolio Page

--- my-demo-project/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("toggle").addEventListener("click", function(){
-    if(document.getElementById("nav-bar").style.display == "none")
-        document.getElementById("nav-bar").style.display = "block";
-    else
-    document.getElementById("nav-bar").style.display = "none"
-});
